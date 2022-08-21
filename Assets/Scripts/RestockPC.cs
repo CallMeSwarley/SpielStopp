472190870765023b8a19f329c7e7e6558e7d9dbc
VIncent Bakels <vincent.bakels@web.de>
2022-08-22 01:25:27 +0200

    Started buy menu

--- Assets/Scripts/RestockPC.cs
+++ Assets/Scripts/RestockPC.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RestockPC : MonoBehaviour, Interactable
 {
+
+    public GameObject BuyMenuUI;
+    private bool inDialog = false;
     public void endInteract()
     {
         
@@ -14,9 +17,20 @@ public class RestockPC : MonoBehaviour, Interactable
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Boop");
+            BuyGames();
         }
     }
+
+    public void BuyGames()
+    {
+        
+        BuyMenuUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        inDialog = Time.timeScale == 0.00001f;
+        Time.timeScale = 0.00001f;//freezes time in game       
+    }
+
 
     public bool IsCurrentlyInteractable()
     {
