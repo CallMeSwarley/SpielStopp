c20921ded7549c7f6ff1e717fb0de236c5d3327c
VIncent Bakels <vincent.bakels@web.de>
2022-08-22 23:31:58 +0200

    Revert "new story method"
    This reverts commit acf05cc9a28ec4f1a8b08332da044030646b0eb2.

--- Assets/Scripts/DialogMenu.cs
+++ Assets/Scripts/DialogMenu.cs
@@ -12,7 +12,6 @@ public class DialogMenu : MonoBehaviour
 	private InteractionManagerNPC interactionManagerNPC;
 	private TextAsset inkJSONAsset;
 	private Action actionAfterDialog=null;
-	private Action actionAfterDialogNegative = null;
 
 	public void startDialog(InteractionManagerNPC interactionManagerNPC, Story story, Action actionAfterDialog = null)
     {
@@ -28,22 +27,7 @@ public class DialogMenu : MonoBehaviour
 		RemoveChildren();
 		StartStory();
 	}
-	public void startDialogComplex(InteractionManagerNPC interactionManagerNPC, Story story, Action actionAfterDialog = null, 
-		Action actionAfterDialogNegative = null)
-	{
-		this.story = story;
-		this.interactionManagerNPC = interactionManagerNPC;
-		if (actionAfterDialog != null) { this.actionAfterDialog = actionAfterDialog; }
-		if (actionAfterDialogNegative != null) { this.actionAfterDialogNegative = actionAfterDialogNegative; }
-		if (story == null)//default dialog
-		{
-			Debug.Log("Couldn't find dialog -> using default");
-			TextAsset ta = (TextAsset)AssetDatabase.LoadAssetAtPath("Assets/Dialoge/justLookingDialogue.json", typeof(TextAsset));
-			this.story = new Story(ta.text);
-		}
-		RemoveChildren();
-		StartStory();
-	}
+	
 	public void startDialog()
     {
 		RemoveChildren();
@@ -91,16 +75,7 @@ public class DialogMenu : MonoBehaviour
 		// If we've read all the content and there's no choices, the story is finished!
 		else
 		{
-			bool positiveEnding = (bool)story.variablesState["positiveEnding"];
-			if (positiveEnding)
-            {
-				StartCoroutine(interactionManagerNPC.endDialog(true, actionAfterDialog));
-            }
-            else
-            {
-				StartCoroutine(interactionManagerNPC.endDialog(true, actionAfterDialogNegative));
-			}
-			
+			StartCoroutine(interactionManagerNPC.endDialog((bool)story.variablesState["positiveEnding"], actionAfterDialog));
 		}
 	}
 	// When we click the choice button, tell the story to choose that choice!
