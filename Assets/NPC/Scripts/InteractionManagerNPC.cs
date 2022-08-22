cbde8ab2ea30818d7ee7c5ceda40e0ff52c516d5
WQHDGamer <86773256+WQHDGamer@users.noreply.github.com>
2022-08-22 22:10:28 +0200

    ja hier

--- Assets/NPC/Scripts/InteractionManagerNPC.cs
+++ Assets/NPC/Scripts/InteractionManagerNPC.cs
@@ -71,7 +71,9 @@ public class InteractionManagerNPC : MonoBehaviour, Interactable
                 story = createStory("Assets/Dialoge/sellGame.json");
                 int randomGameID = UnityEngine.Random.Range(0, ProductDataManager.TitleData.Count - 1);
                 story.variablesState["GameToSell"] = ProductDataManager.TitleData[randomGameID];
-                int preis = (ProductDataManager.TrendData[randomGameID] + ProductDataManager.RatingData[randomGameID]) * 3;
+                UnityEngine.Random random = new Random();
+                int randomNumber = random.Next(2, 5);
+                int preis = (ProductDataManager.TrendData[randomGameID] + ProductDataManager.RatingData[randomGameID]) * randomNumber;
                 story.variablesState["Price"] = preis;
                 startDialog(story);//kassenmenü öffnen
 
